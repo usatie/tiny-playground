c447e792c659826df0b8e9ae785ba339bd0f8c80
Shun Usami <usatie@gmail.com>
2025-07-11 18:16:33 -0700

    Add graph visualization to micrograd

--- micrograd.py
+++ micrograd.py
@@ -19,10 +19,11 @@ plt.show()
 """
 
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
@@ -35,10 +36,13 @@ class Value:
         out = Value(self.data * other.data, (self, other), '*')
         return out
 
-a = Value(2.0)
-b = Value(-3.0)
-c = Value(10.0)
-d = a * b + c
+a = Value(2.0, label='a')
+b = Value(-3.0, label='b')
+c = Value(10.0, label='c')
+e = a*b; e.label = 'e'
+d = e + c; d.label = 'd'
+f = Value(-2.0, label='f')
+L = d * f; L.label = 'L'
 print(a)
 print(a + b)
 print(a.__add__(b))
@@ -48,3 +52,40 @@ print(a.__mul__(b).__add__(c))
 print(d)
 print(d._prev)
 print(d._op)
+
+from graphviz import Digraph
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'})
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        dot.node(name = uid, label = "{ %s | data %.4f }" % (n.label, n.data, ), shape='record')
+        if n._op:
+            dot.node(name = uid + n._op, label = n._op)
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
+draw_dot(d)
+
+# See the graph?
+dot = draw_dot(L)
+dot.render('out/dot')
+
